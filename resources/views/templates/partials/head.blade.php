77dae036064b4be25fd78acc03aeaee7f56362fa
Mochammad Gamal <mogams1811@gmail.com>
2023-08-02 09:59:21 +0700

    fix error ketika load asset css atau javascript dan ganti icon website.

--- resources/views/templates/partials/head.blade.php
+++ resources/views/templates/partials/head.blade.php
@@ -4,8 +4,11 @@
 <title> {{ $title ?? 'Dashboard' }} | {{ env('APP_NAME') }}</title>
 
 <!-- CSS files -->
-<link href="{{ asset('dist/css/tabler.min.css?1674944402') }}" rel="stylesheet" />
-<link href="{{ asset('dist/css/tabler-flags.min.css?1674944402') }}" rel="stylesheet" />
-<link href="{{ asset('dist/css/tabler-payments.min.css?1674944402') }}" rel="stylesheet" />
-<link href="{{ asset('dist/css/tabler-vendors.min.css?1674944402') }}" rel="stylesheet" />
-<link href="{{ asset('dist/css/demo.min.css?1674944402') }}" rel="stylesheet" />
+<link href="{{ asset('dist/css/tabler.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('dist/css/tabler-flags.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('dist/css/tabler-payments.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('dist/css/tabler-vendors.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('dist/css/demo.min.css') }}" rel="stylesheet" />
+
+<!-- setup icon website -->
+<link rel="icon" href="{{ asset('static/graduates.png') }}" type="image/png" sizes="16x16">
--- resources/views/templates/partials/scripts.blade.php
+++ resources/views/templates/partials/scripts.blade.php
@@ -1,8 +1,8 @@
 <!-- Libs JS -->
-<script src="./dist/libs/apexcharts/dist/apexcharts.min.js?1674944402" defer></script>
-<script src="./dist/libs/jsvectormap/dist/js/jsvectormap.min.js?1674944402" defer></script>
-<script src="./dist/libs/jsvectormap/dist/maps/world.js?1674944402" defer></script>
-<script src="./dist/libs/jsvectormap/dist/maps/world-merc.js?1674944402" defer></script>
+<script src="{{ asset('dist/libs/apexcharts/dist/apexcharts.min.js') }}" defer></script>
+<script src="{{ asset('dist/libs/jsvectormap/dist/js/jsvectormap.min.js') }}" defer></script>
+<script src="{{ asset('dist/libs/jsvectormap/dist/maps/world.js') }}" defer></script>
+<script src="{{ asset('dist/libs/jsvectormap/dist/maps/world-merc.js') }}" defer></script>
 <!-- Tabler Core -->
-<script src="./dist/js/tabler.min.js?1674944402" defer></script>
-<script src="./dist/js/demo.min.js?1674944402" defer></script>
+<script src="{{ asset('dist/js/tabler.min.js') }}" defer></script>
+<script src="{{ asset('dist/js/demo.min.js') }}" defer></script>
