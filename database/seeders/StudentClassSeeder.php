070ccb7979b890bc8ed6f3ba840aaca3b3ed12e2
Mochammad Gamal <mogams1811@gmail.com>
2023-08-06 15:03:21 +0700

    tambah seeder student class model.

--- database/seeders/StudentClassSeeder.php
+++ database/seeders/StudentClassSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\StudentClass;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,12 +16,18 @@ class StudentClassSeeder extends Seeder
      */
     public function run()
     {
-        //
-        $classes = [
-            [
-                'name' => 'XII RPL 1',
-                'slug' => 'xii-rpl-1',
-            ],
-        ];
+        $counter = 1;
+        $index = 0;
+        do {
+            $classes[] = [
+                'name' => 'XII RPL ' . $counter,
+                'slug' => 'xii-rpl-' . $counter,
+            ];
+            StudentClass::create($classes[$index]);
+            $counter++;
+            $index++;
+        } while ($counter <= 3);
+        $classes_count = count($classes);
+        $this->command->info('Berhasil menambahkan ' . $classes_count . ' kelas');
     }
 }
