d1027be8a5590f0786d92b02705e752d9e235621
Mochammad Gamal <mogams1811@gmail.com>
2023-08-06 15:10:16 +0700

    migration kolom student class id ke tabel student.

---
+++ database/migrations/2023_08_06_080432_add_student_class_id_on_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            //
+            $table->foreignId('class_id')->after('id')->nullable()->constrained('student_classes')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            //
+            $table->dropForeign(['class_id']);
+        });
+    }
+};
