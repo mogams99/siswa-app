e0beec9e336f88816af60932e8e6cef6bb1b627d
Mochammad Gamal <mogams1811@gmail.com>
2023-08-18 09:22:47 +0700

    fix nama web pada head.

--- resources/views/templates/partials/head.blade.php
+++ resources/views/templates/partials/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
 <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-<title> {{ $title ?? 'Dashboard' }} | {{ env('APP_NAME') }}</title>
+<title> {{ $title ?? 'Dashboard' }} | {{ config('app.name') }}</title>
 
 <!-- CSS files -->
 <link href="{{ asset('dist/css/tabler.min.css') }}" rel="stylesheet" />
