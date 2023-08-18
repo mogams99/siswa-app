a659b3a7aac4ddd9d1c57b47ff2d95497f3d5395
Mochammad Gamal <mogams1811@gmail.com>
2023-08-18 09:23:27 +0700

    setup form create student classes.

--- resources/views/student-classes/create.blade.php
+++ resources/views/student-classes/create.blade.php
@@ -1,6 +1,6 @@
 @extends('templates.default')
 @php
-$title = 'Student';
+$title = 'Student Classes';
 $preTitle = 'Form Create';
 @endphp
 
@@ -8,11 +8,11 @@ $preTitle = 'Form Create';
 <div class="row">
     <div class="col-12">
 
-        <form class="card" action="{{ route('students.store') }}" method="POST" enctype="multipart/form-data">
+        <form class="card" action="{{ route('student-classes.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="card-body">
                 <div class="mb-3">
-                    <label class="form-label required">Name</label>
+                    <label class="form-label required">Name Class</label>
                     <input type="text" class="form-control" @error('name') is-invalid @enderror name="name" value="{{ old('name') }}">
                     @error('name')
                     <small class="form-hint text-danger">{{ $message }}</small>
@@ -21,35 +21,9 @@ $preTitle = 'Form Create';
                     </div>
                 </div>
                 <div class="mb-3">
-                    <label class="form-label">Photo</label>
-                    <input type="file" class="form-control" @error('photo') is-invalid @enderror name="photo" value="{{ old('photo') }}">
-                    @error('photo')
-                    <small class="form-hint text-danger">{{ $message }}</small>
-                    @enderror
-                </div>
-                <div class="mb-3">
-                    <label class="form-label required">Address</label>
-                    <input type="text" class="form-control" @error('address') is-invalid @enderror name="address" value="{{ old('address') }}">
-                    @error('address')
-                    <small class="form-hint text-danger">{{ $message }}</small>
-                    @enderror
-                </div>
-                <div class="mb-3">
-                    <label class="form-label required">Phone</label>
-                    <input type="text" class="form-control" @error('phone_number') is-invalid @enderror name="phone_number" value="{{ old('phone_number') }}">
-                    @error('phone_number')
-                    <small class="form-hint text-danger">{{ $message }}</small>
-                    @enderror
-                </div>
-                <div class="mb-3">
-                    <label class="form-label required">Class</label>
-                    <select name="student_class_id" id="student_class_id" class="form-select @error('student_class_id') is-invalid @enderror" value="{{ old('student_class_id') }}">
-                        <option value="">-- Choose Class --</option>
-                        @foreach ($classes as $class)
-                        <option value="{{ $class->id }}">{{ $class->name }}</option>
-                        @endforeach
-                    </select>
-                    @error('student_class_id')
+                    <label class="form-label required">Slug</label>
+                    <input type="text" class="form-control" @error('slug') is-invalid @enderror name="slug" value="{{ old('slug') }}">
+                    @error('slug')
                     <small class="form-hint text-danger">{{ $message }}</small>
                     @enderror
                 </div>
