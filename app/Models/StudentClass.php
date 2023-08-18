636cb6358467830300a2a6cf7df90b01864ecec3
Mochammad Gamal <mogams1811@gmail.com>
2023-08-18 10:18:11 +0700

    typo kolom relasi students.

--- app/Models/StudentClass.php
+++ app/Models/StudentClass.php
@@ -25,6 +25,6 @@ class StudentClass extends Model
 
     public function students()
     {
-        return $this->hasMany(Student::class, 'class_id', 'id');
+        return $this->hasMany(Student::class, 'student_class_id', 'id');
     }
 }
